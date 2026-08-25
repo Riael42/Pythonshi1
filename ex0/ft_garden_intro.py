5b044739a0a71624036e8be8cdf491e52e6cc34e
Riael <riaeldoestwitch@gmail.com>
2026-08-25 04:04:34 +0200

    init

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+	name = "Long Leaf"
+	height = 75
+	age = 30
+	print(f"=== Welcome to My Garden ===\nPlant: {name}\nHeight: {height} cm\nAge: {age} days\n=== End of Program ===\n")
---
+++ ex3/ft_plant_factory.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+class Plant:
+	#class definition with attributes
+	def __init__(self, name, height, age):
+		self.name = name
+		self.height = height
+		self.age = age
+
+	#class method to print the aforementioned attributes
+	def show(self):
+		print(f"{self.name}: {self.height} cm, {self.age} days old")
+
+if __name__ == "__main__":
+    plants = [
+        Plant("Long Leaf", 75, 30),
+        Plant("Giggle Bush", 62, 42),
+        Plant("Jazz Cabbage", 42, 420),
+        Plant("Funky Fern", 15, 15),
+        Plant("Silly Sprout", 5, 5),
+    ]
+
+    print("==Garden Plant Factory==")
+    for plant in plants:
+        print("Created: ", end="")
+        plant.show()
